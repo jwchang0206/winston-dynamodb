e03b45b1ae381964c14494854f41b7e643226c43
inspiredjw <inspired.jw@gmail.com>
2016-06-09 07:10:36 -0700

    added ap-northeast-2 and eu-central-1

--- winston-dynamodb.js
+++ winston-dynamodb.js
@@ -41,7 +41,7 @@
     if (options == null) {
       options = {};
     }
-    regions = ["us-east-1", "us-west-1", "us-west-2", "eu-west-1", "ap-northeast-1", "ap-southeast-1", "ap-southeast-2", "sa-east-1"];
+    regions = ["us-east-1", "us-west-1", "us-west-2", "eu-west-1", "eu-central-1", "ap-northeast-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "sa-east-1"];
     if (options.useEnvironment) {
       options.accessKeyId = process.env.AWS_ACCESS_KEY_ID;
       options.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
